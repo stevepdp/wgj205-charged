9856a407d129055797ad5e7537e2c1cee7a79393
Stephen Pearce <hello@stevepdp.dev>
2023-06-21 00:07:45 +0100

    feat(enemy): first pass of Enemy class tidying/refactoring

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
